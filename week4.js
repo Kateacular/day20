97e98054fd357403393eed2d0bef25319744dea9
Kateacular <kate.orr@detroitlabs.com>
2017-03-24 14:01:15 -0400

    adding instructions and sget line

---
+++ week4.js
@@ -0,0 +1,27 @@
+
+// 1.) Enter his/her name
+// 2.) Name his/her penguin
+// 3.) Decide if the penguin is male or female
+// 4.) Decide if the penguin is an Emperor, Gentoo, or rockhopper penguin
+
+// The user's penguin should be represented by an object.
+
+
+// Once the player has entered his/her name, 
+// the program should address him/her by name in all future prompts, 
+// and should refer to his/her penguin by name as well.
+
+// - In the game, the user should have to answer simple math questions 
+// (for Version 1 of your game, you can use addition problems only) 
+// to earn a virtual currency known as PenguinPebbles
+// - For each correct answer, the user should get 5 PenguinPebbles
+// - For each incorrect answer, the user should lose 5 PenguinPebbles
+// - When the user gets 20 or more pebbles, 
+// they can "spend" their PenguinPebbles to make their penguin do a trick
+// - The penguin should be able to do at least (3) different tricks, 
+// each of which costs a different number of PenguinPebbles
+
+// ***********
+
+var sget = require("sget");
+
